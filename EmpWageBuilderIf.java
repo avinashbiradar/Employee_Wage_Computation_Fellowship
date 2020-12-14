38917d7e11e1b7d9b18cdb1dfd5af20fbbb89753
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-14 17:56:25 +0530

    Added:Employee Daily Wage

--- EmpWageBuilderIf.java
+++ EmpWageBuilderIf.java
@@ -1,5 +1,25 @@
 public class EmpWageBuilderIf {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+    public static final int IS_PART_TIME=1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR=20;
+    public static void main(String[] args)
+    {
+            int empHrs = 0;
+            int empWage = 0;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                System.out.println("Full Time");
+                System.out.println("empWage " + empWage);
+            }
+            else
+            {
+                empHrs=0;
+                empWage= empHrs*EMP_RATE_PER_HOUR;
+                System.out.println("absent");
+                System.out.println("empWage "+empWage);
+            }
     }
 }
