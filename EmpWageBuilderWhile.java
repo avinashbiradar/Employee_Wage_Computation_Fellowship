b0b4c17ce042b2938271ae1d55ea85dc08485098
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-14 18:22:25 +0530

    Added:Calculate wages till a condition of total working hours

---
+++ EmpWageBuilderWhile.java
@@ -0,0 +1,35 @@
+public class EmpWageBuilderWhile {
+    static final int IS_PART_TIME = 1;
+    static final int IS_FULL_TIME = 2;
+    static final int EMP_RATE_PER_HOUR = 20;
+    static final int NUM_OF_WORKING_DAYS = 20;
+    private static final int MAX_HRS_IN_MONTH =100 ;
+
+    public static void main(String[] args) {
+        int empHrs = 0;
+        int totalEmpWage = 0;
+        int totalWorkingDays=0;
+        int totalEmpHrs=0;
+        while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+        {
+            int empWage = 0;
+            totalWorkingDays++;
+            int empCheck    = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("empWage " + empWage);
+        }
+        System.out.println("TotalEmployeeWage " + totalEmpWage);
+    }
+}
